7f4406e2dacf54d9b6ba34c390316d86e1f04999
Gianmaria <gianmaria.bonici@gmail.com>
2022-12-12 19:30:40 +0100

    progress part 2

--- Day10/src/main.cpp
+++ Day10/src/main.cpp
@@ -2,7 +2,7 @@
 
 
 
-int main()
+int part_1()
 {
     try
     {
@@ -29,9 +29,9 @@ int main()
                 if (cycle == 20 or
                     ((cycle - 20) % 40 == 0))
                 {
-                    /*cout << "cycle: " << cycle 
-                        << " X: " << X 
-                        << " signal strength: " << cycle * X 
+                    /*cout << "cycle: " << cycle
+                        << " X: " << X
+                        << " signal strength: " << cycle * X
                         << endl;*/
                     signal_strength += cycle * X;
                 }
@@ -54,7 +54,7 @@ int main()
 
                 cycle_up(cycle);
                 cycle_up(cycle);
-                
+
                 X += value;
             }
             else
@@ -71,4 +71,103 @@ int main()
     {
         cout << "[EXC] " << e.what() << endl;
     }
+
+    return 1;
 }
+
+
+int main()
+{
+    try
+    {
+#if 1
+        auto file_path = "res/test.txt";
+#else
+        auto file_path = "res/input.txt";
+#endif
+        cout << "[INFO] input file: " << file_path << endl;
+        auto ifs = std::ifstream(file_path);
+
+        if (not ifs.is_open())
+            throw std::runtime_error(str("[ERROR] Cannot open file ") + file_path);
+
+        i32 cycle = 1;
+        i32 X = 1;
+        string line;
+
+        std::array<std::array<char, 40>, 6> screen;
+
+        for (auto& row : screen)
+        {
+            for (auto& pixel : row)
+            {
+                pixel = '.';
+            }
+        }
+
+        auto print_screen = [&screen]()
+        {
+            for (auto& row : screen)
+            {
+                for (auto& pixel : row)
+                {
+                    cout << pixel;
+                }
+                cout << endl;
+            }
+        };
+
+        auto cycle_up = [&]()
+        {
+            i32 h_sprite_pos = X; // ###
+
+            i32 pixel_currently_being_drawn = cycle;
+
+            if (h_sprite_pos - 1 == pixel_currently_being_drawn or
+                h_sprite_pos     == pixel_currently_being_drawn or
+                h_sprite_pos + 1 == pixel_currently_being_drawn)
+            {
+                u32 row = (cycle-1) / 40;
+                u32 col = (cycle-1) % 40;
+                screen[row][col] = '#';
+            }
+
+            ++cycle;
+        };
+
+        while (std::getline(ifs, line))
+        {
+            std::istringstream iss(line);
+
+            str instruction;
+            iss >> instruction;
+
+            if (instruction == "addx")
+            {
+                i32 value;
+                iss >> value;
+
+                cycle_up();
+                cycle_up();
+
+                X += value;
+            }
+            else
+            {
+                cycle_up();
+            }
+        }
+
+        cout << "part 2" << endl;
+        print_screen();        
+
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        cout << "[EXC] " << e.what() << endl;
+    }
+
+    return 1;
+}
+
